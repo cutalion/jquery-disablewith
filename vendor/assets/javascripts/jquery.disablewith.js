63f5f8d83a8459a38e188c0fe00f9ca5d4332249
Alexander Glushkov <cutalion@gmail.com>
2011-12-16 00:21:25 +0200

    Disable click events during the AJAX request

--- vendor/assets/javascripts/jquery.disablewith.js
+++ vendor/assets/javascripts/jquery.disablewith.js
@@ -2,17 +2,22 @@
   $.fn.disableWith = function(options){
 
     var settings = $.extend({
-      text: 'Processing...'
+      text          : 'Processing...',
+      disable_event : function(e){
+        e.preventDefault();
+        return false;
+      }
     }, options);
 
     this.bind('ajax:beforeSend', function(){ 
       $(this)._previous_text = $(this).html();
-      $(this).html(settings.text);
+      $(this).html(settings.text).click(settings.disable_event);
     });
 
     this.bind('ajax:complete', function(){ 
-      $(this).html($(this)._previous_text);
+      $(this).html($(this)._previous_text).unbind(click, settings.disable_event);
     });
+
 
     return this;
   };
